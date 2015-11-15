77395f3dc0266958be6454cc431e849bb4dc4daa
Craig Campbell <craig.campbell8@gmail.com>
2015-11-14 16:13:10 -0800

    routes file now has a way to store photos  as base 64 strings in the db at the bottom of the file

--- models/photo.js
+++ models/photo.js
@@ -1,10 +1,13 @@
 var mongoose = require('mongoose');
 
 var photoSchema = new mongoose.Schema({
-	imgname: {type: String,
-	altered: {type: Boolean, default: 'false'},
-	caption: {type: String, default: 'photo description'}
-	}
+  imgName: String,
+  imgData: String
+
+	// imgname: {type: String,
+	// altered: {type: Boolean, default: 'false'},
+	// caption: {type: String, default: 'photo description'}
+	// }
 });
 
 module.exports = mongoose.model('Photo', photoSchema);
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -40,3 +40,32 @@ usersRouter.delete('/users/:id', function(req, res) {
 	});
 });
 
+///////// this saves a base64-encoded string of the photo data in our database upon a POST requuest. You need to post '{"imgName":"whatevernameyouwant","imgData":"thisstringcanbeanythingnbecauseitwillbeoverwrittenanyway"}' . the data will be converetd to an actual image upon a subsequent GET request to /api/photo/:imgName
+var Photo = require(__dirname + '/../models/photo');
+var fs = require('fs');
+var buf1 = fs.readFileSync(__dirname + '/../img_data/normal_colour.png');
+var string64 = buf1.toString('base64');
+
+usersRouter.post('/photo', bodyParser.json(), function(req, res) {
+	req.body.imgData = string64;
+	var newPhoto = new Photo(req.body);
+
+	newPhoto.save(function(err, data) {
+		if (err) return handleError(err, res);
+
+		res.json("data has been saved in the database");
+	});
+});
+
+///// ok now you get to CREATE the photo image upon a get request: The file is not saved until after you make the get request and the string data for image is pulled from the db
+
+usersRouter.get('/photo/:imgName', function(req, res) {
+	Photo.find({imgName: req.params.imgName}, function(err, data) {
+		if (err) return handleError(err, res);
+		var newBuf = new Buffer(data[0].imgData, 'base64');
+
+		fs.writeFileSync(__dirname + '/../img_data/photo999.png', newBuf);
+		res.send(data)
+
+	});
+});
