b67caca695f4445c0f3073ed407f9eabb68abb5b
Craig Campbell <craig.campbell8@gmail.com>
2015-11-18 09:39:37 -0800

    push to heroku

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT || 3000;
-// var usersRouter = require(__dirname + '/routes/users_routes.js');
-// var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/user_dev';
-// var mongoose = require('mongoose');
-// mongoose.connect(mongoURI);
+var usersRouter = require(__dirname + '/routes/users_routes.js');
+var mongoURI = process.env.MONGOLAB_URI || 'mongodb://localhost/user_dev';
+var mongoose = require('mongoose');
+mongoose.connect(mongoURI);
 
-// app.use('/api', usersRouter);
+app.use('/api', usersRouter);
 
 app.get('/', function(req, res) {
   res.send('howdy')
