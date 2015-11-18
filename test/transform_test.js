0d97776e3abf5d7e7254e362b4862914df8d3c25
Rick Patci <rpatci@gmail.com>
2015-11-18 13:53:49 -0800

    nov18 update

---
+++ test/transform_test.js
@@ -0,0 +1,17 @@
+var expect = require('chai').expect;
+require(__dirname + '/../lib/jpeg');
+
+describe('the transform process', function() {
+
+  it('should convert a single rgb value to hex', function() {
+    expect(componentToHex('255').to.not.eql('255'));
+  });
+
+  it('should convert a full RGB val to hex', function() {
+    expect(rgbToHex('255', '0', '50').to.not.eql('255', '0', '50'));
+  });
+
+  it('should transform a hex value', function() {
+    expect(protan('#FEF571').to.not.eql('#FEF571'));
+  });
+});
