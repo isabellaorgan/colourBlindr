dd561ebc74c43984ed2e2bc2c38ae6efe547fea8
Rick Patci <rpatci@gmail.com>
2015-11-19 14:28:01 -0800

    add two tests

--- test/server-test.js
+++ test/server-test.js
@@ -42,6 +42,35 @@ describe('the server', function(){
     });
   });
 
+  it('should GET a registered end point', function(done){
+    chai.request('localhost:3000')
+    .get('/original')
+    .end(function(err, res){
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      done();
+    });
+  });
+
+  it('should GET a registered end point', function(done){
+    chai.request('localhost:3000')
+    .get('/transformed')
+    .end(function(err, res){
+      expect(err).to.eql(null);
+      expect(res.status).to.eql(200);
+      done();
+    });
+  });
+
+  it('should send a 404 to an unregistered end point', function(done){
+    chai.request('localhost:3000')
+    .get('/5fbhjbu')
+    .end(function(err, res){
+      expect(res.status).to.eql(404);
+      done();
+    });
+  });
+
   it('should POST users to the DB', function(done){
     chai.request('localhost:3000')
     .post('/api/users')
