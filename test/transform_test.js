dba4a2e10045ab7b9ef7bb6a894a52c7b3702351
Rick Patci <rpatci@gmail.com>
2015-11-19 10:04:48 -0800

    2 more tests pass

--- test/transform_test.js
+++ test/transform_test.js
@@ -7,14 +7,14 @@ var Jimp = require('patci-jimp');
 describe('the transform process', function() {
 
   it('should convert a single rgb value to hex', function() {
-    expect(componentToHex('255').to.not.eql('255'));
+    expect(componentToHex(255)).to.not.eql(255);
   });
 
   it('should convert a full RGB val to hex', function() {
-    expect(rgbToHex('255', '0', '50').to.not.eql('255', '0', '50'));
+    expect(rgbToHex('255', '0', '50')).to.not.eql('255', '0', '50');
   });
 
   it('should transform a hex value', function() {
-    expect(protan('#FEF571').to.not.eql('#FEF571'));
+    expect(protan('#FEF571')).to.not.eql('#FEF571');
   });
 });
